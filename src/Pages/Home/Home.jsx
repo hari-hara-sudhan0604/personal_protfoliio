e00fc0eeadc1da50245650e380b845ac36cd5b30
hari-hara-sudhan0604 <coolhari0604@gmail.com>
2026-07-13 12:45:38 +0530

    Build hero section with professional layout

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,10 @@
+import Hero from "../../components/Hero/Hero";
+
 function Home() {
   return (
-    <section>
-      <h1>Hello, I'm Hari 👋</h1>
-
-      <h2>Full Stack Developer</h2>
-
-      <p>
-        Welcome to my portfolio website.
-      </p>
-    </section>
+     <>
+      <Hero />
+    </>
   );
 }
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,82 @@
-function Navbar() {
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import "./Hero.css";
+
+function Hero() {
   return (
-    <nav>
-      Hero
-    </nav>
+    <section className="hero section">
+      <div className="container hero-container">
+
+        {/* Left Content */}
+        <div className="hero-content">
+
+          <p className="hero-greeting">
+            👋 Hello, I'm
+          </p>
+
+          <h1>
+            Hari
+            <span> Kumar</span>
+          </h1>
+
+          <h2>
+            Full Stack MERN Developer
+          </h2>
+
+          <p className="hero-description">
+            Passionate Full Stack Developer with experience
+            in building responsive, scalable and user-friendly
+            web applications.
+
+            This is dummy text.
+
+            Replace this later with your own professional
+            summary.
+          </p>
+
+          <div className="hero-buttons">
+
+            <button className="primary-btn">
+              Hire Me
+            </button>
+
+            <button className="secondary-btn">
+              Download Resume
+            </button>
+
+          </div>
+
+          <div className="hero-social">
+
+            <a href="#">
+              <FaGithub />
+            </a>
+
+            <a href="#">
+              <FaLinkedin />
+            </a>
+
+            <a href="#">
+              <FaEnvelope />
+            </a>
+
+          </div>
+
+        </div>
+
+        {/* Right Content */}
+
+        <div className="hero-image">
+
+          <img
+            src="https://placehold.co/500x500"
+            alt="Hari Portfolio"
+          />
+
+        </div>
+
+      </div>
+    </section>
   );
 }
 
-export default Navbar;
+export default Hero;
